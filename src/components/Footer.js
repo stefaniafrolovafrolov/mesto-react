9fdfa6c57ac6f38ff131e608d874e6ed4dc7d2d1
Vadim Frolov <stefaniafrolovafrolov@yandex.ru>
2023-03-23 12:41:23 +0300

    готовый проект 10 работа Mesto-React после внесение правок

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,11 @@ import React from "react"
 
 function Footer() {
   return (
-    <div>
-      <footer className="footer">
-        <p className="footer__copyright">© 2022. Фролов Вадим</p>
-      </footer>
-    </div>
+    <footer className="footer">
+      <p className="footer__copyright">
+        © {new Date().getFullYear()}. Фролов Вадим
+      </p>
+    </footer>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,13 @@ import headerLogo from "../images/header-logo.svg"
 
 function Header() {
   return (
-    <div>
-      <header className="header">
-        <img
-          className="header__logo"
-          src={headerLogo}
-          alt="Надписть на логотипе Mesto Russia"
-        />
-      </header>
-    </div>
+    <header className="header">
+      <img
+        className="header__logo"
+        src={headerLogo}
+        alt="Надписть на логотипе Mesto Russia"
+      />
+    </header>
   )
 }
 

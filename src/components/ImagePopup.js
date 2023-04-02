13d912a127592bd52f23741d99e7246cb16b90fa
Vadim Frolov <stefaniafrolovafrolov@yandex.ru>
2023-04-03 00:24:52 +0300

    закрытие попапов по Overlay

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -9,11 +9,7 @@ function ImagePopup({ card, onClose, onCloseOverlay }) {
       <figure className="popup__image-container">
         <img className="popup__image" src={card.link} alt={card.name} />
         <figcaption className="popup__image-title">{card.name}</figcaption>
-        <button
-          className="popup__close"
-          type="button"
-          onClick={onClose}
-        ></button>
+        <button className="popup__close" type="button" onClick={onClose} />
       </figure>
     </div>
   )
